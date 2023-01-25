6102632153c0836e930994ed04ad551ffbc869ed
Anderson Silva <anderson.silva1996@hotmail.com>
2023-01-25 20:59:08 +0000

    initial commit

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = "1554b84bb947f17d30f778df9b32d819"
+let img_url = "https://image.tmdb.org/t/p/w500/"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movies_genres_http ="https://api.themoviedb.org/3/discover/movie?"
